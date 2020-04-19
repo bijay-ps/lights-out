d25cbc49700cf7d3a600096b910a67461d3dba2c
BIJAY SINGH <bijay.prakash7@yahoo.in>
2020-04-19 13:11:05 +0530

    Updates

---
+++ precache-manifest.d2075651ef0a9a76437501177429db51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e945e004010c765681064d2d3f88b67a",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "1f5ff427d5d1e3ca6ca4",
+    "url": "/lights-out/static/css/main.106bc289.chunk.css"
+  },
+  {
+    "revision": "3e8b0445151fd00f917e",
+    "url": "/lights-out/static/js/2.2869eace.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lights-out/static/js/2.2869eace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5ff427d5d1e3ca6ca4",
+    "url": "/lights-out/static/js/main.3b789032.chunk.js"
+  },
+  {
+    "revision": "488f2f142fb12499be57",
+    "url": "/lights-out/static/js/runtime-main.5ada5c9a.js"
+  }
+]);
